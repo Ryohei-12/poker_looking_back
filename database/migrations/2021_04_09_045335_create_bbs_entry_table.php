7a278f21cbbfc52b376f7cff829b707b720b522c
Ryohei <>
2021-04-10 21:02:46 +0900

    マイグレーションファイルのエラー修正

--- database/migrations/2021_04_09_045335_create_bbs_entry_table.php
+++ database/migrations/2021_04_09_045335_create_bbs_entry_table.php
@@ -17,18 +17,18 @@ class CreateBbsEntryTable extends Migration
             $table->bigIncrements('id');
             $table->string("title");	//タイトル
             $table->string("author");	//投稿者名
-            $table->string("body");	//本文
-            $table->string("hand"); //ハンド
+            $table->string("body");     //本文
+            $table->string("hand");     //ハンド
             $table->string("position"); //ポジション
-            $table->string("stack"); //有効スタック
-            $table->string("flop"); //フロップ
+            $table->string("stack");    //有効スタック
+            $table->string("flop");     //フロップ
             $table->string("action_at_flop"); //フロップのアクション
-            $table->string("turn"); //ターン
+            $table->string("turn");     //ターン
             $table->string("action_at_turn"); //ターンのアクション
-            $table->string("river"); //リバー
+            $table->string("river");    //リバー
             $table->string("action_at_river"); //リバーのアクション
             $table->string("hand_of_opponent"); //ショーダウン
-            $table->string("result"); //収支
+            $table->string("result");   //収支
             $table->timestamps();
         });
     }
