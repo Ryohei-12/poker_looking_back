2e76daddcda9992cf333086a9c25f901f8d7ac5e
Ryohei <>
2021-04-10 20:47:17 +0900

    ミスコミット

--- database/migrations/2021_04_09_045335_create_bbs_entry_table.php
+++ database/migrations/2021_04_09_045335_create_bbs_entry_table.php
@@ -17,12 +17,12 @@ class CreateBbsEntryTable extends Migration
             $table->bigIncrements('id');
             $table->string("title");	//タイトル
             $table->string("author");	//投稿者名
-            $table->string("body");	//本文
-            $table->string("hand"); //ハンド
-            $table->string("position"); //ポジション
-            $table->string("stack"); //有効スタック
-            $table->string("flop"); //フロップ
-            $table->string("action_at_flop"); //フロップのアクション
+            $table->string("body");	　//本文
+            $table->string("hand"); 　//ハンド
+            $table->string("position"); 　//ポジション
+            $table->string("stack"); 　//有効スタック
+            $table->string("flop"); 　//フロップ
+            $table->string("action_at_flop"); 　//フロップのアクション
             $table->string("turn"); //ターン
             $table->string("action_at_turn"); //ターンのアクション
             $table->string("river"); //リバー
