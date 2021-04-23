8243d11aac0c7472dd2a51c5f8bdca73662fbb05
Ryohei <>
2021-04-23 21:22:13 +0900

    UI整え始めた

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,8 +3,19 @@
 @section('title', 'メインメニュー')
 	@include('nav')
 
-    <button onclick="location.href='/articles/create'">ハンドレビュー投稿</button>
+	<div style="text-align: center;">
+		<div>
+			気になったハンドを投稿する</br>
+			<button style="color:ivory; background-color:black; font-size:20px; width:160px; height: 60px; margin:10px" onclick="location.href='/articles/create'">ハンドレビュー投稿</button>
+		</div>
 
-	<button onclick="location.href='/articles/index'">ハンドレビュータイムライン</button>
+		<div>
+			みんなのハンド履歴をチェック</br>
+			<button style="color:ivory; background-color:black; font-size:20px; width:160px; height: 60px; margin:10px" onclick="location.href='/articles/index'">ハンドレビュータイムライン</button>
+		</div>
 
-	<button onclick="location.href='/handrange'">プリフロップレンジ確認</button>
+		<div>
+			プリフロップレンジ表を確認</br>
+			<button style="color:ivory; background-color:black; font-size:20px; width:160px; height: 60px; margin:10px" onclick="location.href='/range'">プリフロップレンジ確認</button>
+		</div>
+	</div>
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -4,19 +4,37 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 	<title>Document</title>
+
+	<style type="text/css">
+	.jumbotron {url(img/background.PNG) z-index:1; center no-repeat; background-size: cover;}
+	</style>
+	
 </head>
 <body>
-	<h1>Poker looking back</h1>
+<div class="jumbotron" style="background-color: black; z-index:3">
+	<h1 class="text-center text-light">Poker looking back</h1>
 	
-		<!-- 投稿ページへ遷移 -->
-		<button onclick="location.href='/login'">ログイン</button>
-		<button onclick="location.href='/register'">新規登録</button>
-	<!-- トップコメント -->
-	<p>
-	ポーカーの上達に必須のハンドレビュー。<br>
-	それだけのためのコミュニティを作りました。
-	</p>
+	<h5 class="text-center text-light">~ by Poker player for Poker player ~</h5>
+
+	<h3 class="text-center text-light">スキルアップしたいか？</h3>
+	<p class="text-center text-light">ポーカーのスキルアップに必須の</br>
+	レビューに特化した唯一のコミュニティ。</p>
+
+	<div style="text-align: center;">
+		<button style="color:ivory; background-color:black; font-size:20px; width:160px; height: 60px; margin:10px" onclick="location.href='/login'">ログイン</button>
+		</br>
+		<button style="color:ivory; background-color:black; font-size:20px; width:160px; height: 60px; margin:10px" onclick="location.href='/register'">新規登録</button>
+	</div>
+
+	<h3 class="text-center text-light">印象に残ったハンドを記録</h3>
+	<p class="text-center text-light">気になったスポットをその場で保存。</br>
+	後の座学の際にスムーズに見返すことが可能に。</p>
+
+	<h3 class="text-center text-light">他者からのハンドレビュー</h3>
+	<p class="text-center text-light">他のプレイヤーからのハンドレビューも受けられることでさらなるスキルアップを。</p>
 
 </body>
 </html>
