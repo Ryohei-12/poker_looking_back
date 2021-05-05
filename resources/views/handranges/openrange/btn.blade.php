1d59621304ced46b88a37a37c5ace98c75f1b266
Ryohei <>
2021-05-06 02:52:50 +0900

    背景画像のコンフコード修正

--- resources/views/handranges/openrange/btn.blade.php
+++ resources/views/handranges/openrange/btn.blade.php
@@ -14,7 +14,7 @@
 
 				<h2>Open Raise Range From BTN</h2>
 
-				<img src="{{ asset('img/open_BTN.png') }}" alt="">
+				<img src="{{ asset('/img/open_BTN.png') }}" alt="">
 				<br/>
 
 				<p class="mt-3">※orange = Raise</p>
