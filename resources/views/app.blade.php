c96b2c17cfd3087f707fcc935477ad7934c4963b
Ryohei <>
2021-05-07 09:57:34 +0900

    アセットコンパイルとjumbotronの背景設定変更

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,7 +18,7 @@
   <!-- jumbotron -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <!--app/css参照-->
-  <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+  <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 
   <!--各bladeファイルでcssを指定-->
   @yield('css')
