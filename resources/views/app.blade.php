43ca5c41902f5b07202bd9a2e2d4d0d8bc20cf90
Ryohei <>
2021-05-07 13:13:16 +0900

    extend.css追加　app.cssに反映（リセットCSSのフォントサイズ等削除）

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,6 +19,8 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <!--app/css参照-->
   <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+  <!--extend.css参照-->
+  <link rel="stylesheet" href="{{ asset('/css/extend.css') }}">
 
   <!--各bladeファイルでcssを指定-->
   @yield('css')
